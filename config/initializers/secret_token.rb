6c2b3f800cd2864ac72cb2651ca23922a4cbf534
reperezz <ricardo@aspgems.com>
2012-11-08 19:33:07 +0100

    Revert "Initial commit"
    This reverts commit 5928ab946dc7da4e4e6c11bbc5fd92564e2b3896.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '3f9f93f44bac75570e6951d8d2bd8566554dc5f163415911340da0dc11bf28ef2035596e7522d26920ece599184713a1c636cf4fbdbce0fb4f1874317fcbafe4'
